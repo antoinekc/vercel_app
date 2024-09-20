5dd681283f4a9ea9dd857f1c7e04e85ce69de02a
antoinekc <kimkimkimkim0719@gmail.com>
2024-09-20 15:11:17 +0200

    commit

--- routes/year.js
+++ routes/year.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 router.get('/', (req, res) => {
   const currentYear = new Date().getFullYear();
-  res.json({year: currentYear.toString() });
+  res.json({ year: currentYear.toString() });
 });
 
 module.exports = router;
